e2b55d422f612a15450b904b779104764bf222d4
Richard Galvez <richardagalvez@gmail.com>
2014-12-13 01:48:05 -0500

    Added the Julia fractal example

--- src/CudaCFD.cu
+++ src/CudaCFD.cu
@@ -1,50 +1,81 @@
-#include <iostream>
+#include "../../cuda_by_example/common/cpu_bitmap.h"
+#include <cmath>
 
-using namespace std;
+#define DIM 800
 
-#define N 10000
+struct cuComplex {
+    float   r;
+    float   i;
+    __device__ cuComplex( float a, float b ) : r(a), i(b)  {}
 
-__global__
-void add(int *a, int *b, int *c) {
+    __device__ float magnitude2( void ) {
+        return r * r + i * i;
+    }
 
-	int tid = blockIdx.x;
+    __device__ cuComplex operator*(const cuComplex& a) {
+        return cuComplex(r*a.r - i*a.i, i*a.r + r*a.i);
+    }
 
-	if (tid < N)
-		c[tid] = a[tid] + b[tid];
+    __device__ cuComplex operator+(const cuComplex& a) {
+        return cuComplex(r+a.r, i+a.i);
+    }
 
+};
+
+__device__ int julia( int x, int y, float scale ) {
+    float jx = scale * (float)(DIM/2 - x)/(DIM/2);
+    float jy = scale * (float)(DIM/2 - y)/(DIM/2);
+
+    cuComplex c(-0.8, 0.156);
+    cuComplex a(jx, jy);
+
+    int i = 0;
+    for (i=0; i<200; i++) {
+
+        a = a * a  + c;
+        if (a.magnitude2() > 1000)
+            return 0;
+    }
+
+    return 1;
 }
 
-int main(void) {
+__global__ void kernel( unsigned char *ptr ) {
+    // map from blockIdx to pixel position
+    int x = blockIdx.x;
+    int y = blockIdx.y;
+    int offset = x + y * gridDim.x;
 
-	int a[N], b[N], c[N];
-	int *dev_a, *dev_b, *dev_c;
-
-	//allocate the memory on the GPU
-	cudaMalloc( (void**)&dev_a, N * sizeof(int) );
-	cudaMalloc( (void**)&dev_b, N * sizeof(int) );
-	cudaMalloc( (void**)&dev_c, N * sizeof(int) );
-
-	for (int i = 0; i < N; i++) {
-		a[i] = -i;
-		b[i] = i * i;
-	}
-
-	cudaMemcpy( dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice );
-	cudaMemcpy( dev_b, b, N * sizeof(int), cudaMemcpyHostToDevice );
-
-	add<<<N, 1>>>( dev_a, dev_b, dev_c );
-
-	cudaMemcpy( c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
-
-	for (int i = 0; i < N; i++) {
-		cout << a[i] << " + " << b[i] << " = " << c[i] << endl;
-	}
-
-	cudaFree( dev_a );
-	cudaFree( dev_b );
-	cudaFree( dev_c );
-
-	return 0;
+    // now calculate the value at that position
+    int juliaValue = julia( x, y, 0.7 );
+    ptr[offset*4 + 0] = 255 * juliaValue;
+    ptr[offset*4 + 1] = 0;
+    ptr[offset*4 + 2] = 0;
+    ptr[offset*4 + 3] = 255;
 }
 
+// globals needed by the update routine
+struct DataBlock {
+    unsigned char   *dev_bitmap;
+};
 
+int main( void ) {
+    DataBlock   data;
+    CPUBitmap bitmap( DIM, DIM, &data );
+    unsigned char    *dev_bitmap;
+
+    cudaMalloc( (void**)&dev_bitmap, bitmap.image_size() );
+    data.dev_bitmap = dev_bitmap;
+
+    dim3    grid(DIM,DIM);
+    kernel<<<grid,1>>>( dev_bitmap );
+
+    cudaMemcpy( bitmap.get_ptr(), dev_bitmap,
+                              bitmap.image_size(),
+                              cudaMemcpyDeviceToHost );
+
+    cudaFree( dev_bitmap );
+
+
+    bitmap.display_and_exit();
+}
